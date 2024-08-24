f66cfaf09cfc5a3e40d429d56df2cf399d3a7163
camiperalta95 <peraltacamila861@gmail.com>
2024-08-24 13:21:44 -0300

    commit

--- index.php
+++ index.php
@@ -4,3 +4,45 @@ require_once 'modelo/Proveedor.php';
 require_once 'modelo/Producto.php';
 require_once 'modelo/PedidoCompra.php';
 require_once 'modelo/LineaCompra.php';
+
+$p= New Proveedor ();
+$p->Id=1;
+$p->Descripcion= 'camila';
+
+$pro1= New Producto ();
+$pro1->Codigo= '123';
+$pro1->Descripcion= 'Masitas';
+$pro1->PrecioVenta= '800';
+
+$pro2= New Producto ();
+$pro2->Codigo= '545';
+$pro2->Descripcion= 'Gaseosa';
+$pro2->PrecioVenta= '2500';
+
+$Li1= New LineaCompra ();
+$Li1->Cantidad= '2';
+$Li1->CostoUnitario= '1500';
+$Li1->FueEntragado= 'true';
+$Li1->Pedido=$pro1;
+
+
+$Li2= New LineaCompra ();
+$Li2->Cantidad= '3';
+$Li2->CostoUnitario= '1200';
+$Li2->FueEntregado='true';
+$Li2->Pedido=$pro2;
+
+
+$pdc= New PedidoCompra ();
+$pdc->Fecha= '12/8/2024';
+$pdc->Proveedor='camila';
+$pdc->FechaEntregaEstimada='20/8/2024';
+$pdc->CostoTotal= '2000';
+$pdc->PedidoCerrado='true';
+
+$pdc->ListLineaCompra[]=$Li1;
+$pdc->ListLineaCompra[]=$Li2;
+
+$pdc->MostrarDatos();
+
+
--- modelo/PedidoCompra.php
+++ modelo/PedidoCompra.php
@@ -9,4 +9,44 @@ class PedidoCompra {
     public $ListLineaCompra = array ();
 
 
+    public function MostrarDatos(){
+
+        echo 'Pedido de compra <br>';
+        echo '-----------------------<br>';
+
+        echo 'Fecha:' . $this->Fecha . '<br>';
+        echo 'Proveedor:' . $this->Proveedor . '<br>';
+        echo 'Fecha de entrega estimada:' . $this->FechaEntregaEstimada . '<br>';
+        echo 'Costo total:' . $this->CostoTotal . '<br>';
+        echo 'Pedido cerrado:' . $this->PedidoCerrado . '<br>';
+
+        echo '------------------------<br>';
+        echo 'Proveedor: <br>';
+        echo '------------------------<br>';
+        echo 'Id:' . $this->Proveedor->Id . '<br>';
+        echo 'Descripcion:' . $this->Proveedor->Descripcion . '<br>';
+
+        echo '---------------------------<br>';
+        echo 'Lista linea de compra: <br>';
+        echo '<br>';
+        
+        foreach ($this->ListLineaCompra as $Lista){
+
+            echo '<br>';
+            echo 'Producto:' . $Lista->Producto . '<br>';
+            echo 'Cantidad:' . $Lista->Cantidad . '<br>';
+            echo 'Costo:' . $Lista->Costo . '<br>';
+            echo 'Entregado:' . $Lista->FueEntregado . '<br>';
+            
+            echo '<br>';
+            echo '-----------------------<br>';
+            echo 'Producto: <br>';
+            echo 'Descripcion:' . $this->Producto->Descripcion . '<br>';
+            echo 'Precio de venta:' . $this->Producto->PrecioVenta . '<br>';
+
+        }
+    }
+
+
+
 }
